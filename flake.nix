21b4842481a3d497af0525035f3e44c732bcca19
archiejudd <archie.judd@yahoo.com>
2025-02-27 00:20:41 +0000

    flake update

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
